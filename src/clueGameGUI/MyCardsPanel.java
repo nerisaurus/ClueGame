0ed0b81ad5e9e528e834f48a36c555f4a3faed34
Nathan Neri <nneri@mymail.mines.edu>
2013-11-06 22:25:57 -0700

    Now the cards don't have the jarring grey border.  Also, we now use icons to mark which type of card it is.

--- src/clueGameGUI/MyCardsPanel.java
+++ src/clueGameGUI/MyCardsPanel.java
@@ -1,10 +1,13 @@
 package clueGameGUI;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Font;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
@@ -20,6 +23,7 @@ import clueGame.Player;
 
 public class MyCardsPanel extends JPanel{
 	JTextField displayCard;
+	JLabel cardIcon;
 	public MyCardsPanel() {
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		setBackground(Color.BLACK);
@@ -32,28 +36,45 @@ public class MyCardsPanel extends JPanel{
 			//First we describe the card:
 			String cardDescription = "";
 			
+			//Then pick its image:
+			cardIcon = new JLabel();
+			cardIcon.setBackground(Color.BLACK);
+			cardIcon.setForeground(p.getColor());
+			
 			switch(card.getType()) {
 			case ROOM:
-				cardDescription += "(room)" + "   ";
+				cardIcon.setIcon(new ImageIcon("roomCardIcon.png"));
+				//cardDescription += "(room)" + "   ";
 				break;
 			case PERSON:
-				cardDescription += "(person)" + " ";
+				cardIcon.setIcon(new ImageIcon("personCardIcon.png"));
+				//cardDescription += "(person)" + " ";
 				break;
 			case WEAPON:
-				cardDescription += "(weapon)" + " ";	
+				cardIcon.setIcon(new ImageIcon("weaponCardIcon.png"));
+				//cardDescription += "(weapon)" + " ";	
 				break;
 			}
 			cardDescription += card.getName();
 			
-			//Now we put that into a JTextField:
+			//Now we put the description we made into a JTextField
 			displayCard = new JTextField();
 			displayCard.setBackground(Color.BLACK);
 			displayCard.setForeground(p.getColor());
 			displayCard.setEditable(false);
+			displayCard.setBorder(BorderFactory.createMatteBorder(0, 0, 5, 0, Color.black));
 			displayCard.setText(cardDescription);
 			
+			//And add it and the icon into a JPanel
+			JPanel cardLine = new JPanel();
+			cardLine.setBackground(Color.BLACK);
+			cardLine.setForeground(p.getColor());
+			cardLine.setLayout(new FlowLayout(FlowLayout.LEFT));
+			cardLine.add(cardIcon);
+			cardLine.add(displayCard);
+			
 			//And finally add that to this Panel
-			add(displayCard);
+			add(cardLine);
 		}
 		//borders
 		setBorder(new TitledBorder (BorderFactory.createLineBorder(p.getColor()),"My Cards", 0, 0, new Font("Arial Narrow", Font.BOLD, 12), p.getColor()));
